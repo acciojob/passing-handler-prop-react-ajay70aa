a07d09b12065ed4d019bca852004979b508ad444
Ajay70aa <ajay70aa@gmail.com>
2025-05-19 17:46:58 +0530

    first commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useState } from 'react'
 
+const Selection = (props) => {
+  const [selectedStyle, updateSelectionStyle] = useState("")
+  const styles = {
+    backgroundColor: selectedStyle,
+  }
+  
+  return (
+    <div>
+      <div className='fix-box' style={styles} onClick={() => props.applyColor((backObj) => updateSelectionStyle(backObj.background))}></div>
+    </div>
+  )
+}
+
+export default Selection
